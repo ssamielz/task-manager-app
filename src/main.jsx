56cddd507ea8cc40ef126b2d1072179e6cabfd50
Sam <ssamsanya3@gmail.com>
2025-04-27 14:36:22 +0300

    main files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter} from 'react-router-dom'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
+import { routes } from './routes'
+import { TaskProvider } from './context/taskContext'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter(routes);
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <TaskProvider>
+      <RouterProvider router={router}/>
+  </TaskProvider>
+    
 )
